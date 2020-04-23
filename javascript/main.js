efa96aa96b6eaf0589c154514a38f112921c4caf
Luca Stefani <theblackloki@gmail.com>
2020-04-23 21:35:41 +0200

    Milestone 3

--- javascript/main.js
+++ javascript/main.js
@@ -7,6 +7,8 @@ $(document).ready(function(){
     $(".libreriaSerie").html("")
     var film_serieCercato = $("#zerca").val()
     console.log(film_serieCercato);
+
+
     $.ajax({
       url:"https://api.themoviedb.org/3/search/movie",
       method:"GET",
@@ -26,6 +28,8 @@ $(document).ready(function(){
         alert("Chiamata fallita!")
       }
     })
+
+
     $.ajax({
       url:"https://api.themoviedb.org/3/search/tv",
       method:"GET",
@@ -48,57 +52,80 @@ $(document).ready(function(){
   })
 
 
-  function lingua(lingua) {
-    var bandieraLingua = ["en","it","de","es","cn","pt","jp"]
-    if (bandieraLingua.includes(lingua)) {
-      return '<img src="img/' + lingua + '.png" alt="' + lingua + '" class="bandierina">';
-      console.log("bandiera");
-    }else {
-      return lingua
-      console.log("no bandiera");
-    }
-    console.log("ma che cazzo");
-  }
-
-  function generaFilm(a) {
-    for (var i = 0; i < a.length; i++) {
-      var filmserie = a[i]
-      // stelline
-      var stellinaVoto = []
-      votoFilmStelle = Math.floor(filmserie.vote_average/2)
-      for (var y = 0; y < 5; y++) {
-        if (votoFilmStelle > y) {
-          stellinaVoto.push("fas")
-        }else {
-          stellinaVoto.push("far")
-        }
-      }
-      // bandierine
-
+  function generaFilm(film) {
+    for (var i = 0; i < film.length; i++) {
+      var filmserie = film[i]
       // serie o film
-      console.log(filmserie.name);
       if (typeof filmserie.name == 'undefined') {
-        $(".libreriaFilm").append(html)
+        var copertina = filmserie.poster_path
+
         var context = {
           tipo:"del film",
           title:filmserie.title,
           originalTitle:filmserie.original_title,
           language:lingua(filmserie.original_language),
-          vote:stellinaVoto
+          vote:starVote(filmserie.vote_average),
+          sitocopertina:'src="https://image.tmdb.org/t/p/',
+          dimensionecopertina:"w185/",
+          immaginecopertina:copertina
         }
+        if (copertina === null) {
+          context.sitocopertina = 'class="notfound"src ="img/notfound.jpg" alt="image not found"'
+          context.dimensionecopertina = ""
+        }
+        console.log("sono un film");
+        console.log(context);
         var html = template(context)
+        $(".libreriaFilm").append(html)
       }else {
-        $(".libreriaSerie").append(html)
+        var copertina = filmserie.poster_path
         var context = {
           tipo:"della serie",
-          nomeserie:filmserie.name,
+          title:filmserie.name,
           originalTitle:filmserie.original_name,
           language:lingua(filmserie.original_language),
-          vote:stellinaVoto
+          vote:starVote(filmserie.vote_average),
+          sitocopertina:'src="https://image.tmdb.org/t/p/',
+          dimensionecopertina:"w185/",
+          immaginecopertina:copertina
         }
+        if (copertina === null) {
+          context.sitocopertina = 'class="notfound"src ="img/notfound.jpg" alt="image not found"'
+          context.dimensionecopertina = ""
+        }
+        console.log("sono una serie");
+        console.log(context);
         var html = template(context)
+        $(".libreriaSerie").append(html)
       }
 
     }
+  }
+  // bandierine
+  function lingua(lingua) {
+    var bandieraLingua = ["en","it","de","es","cn","pt","jp"]
+    if (bandieraLingua.includes(lingua)) {
+      return '<img src="img/' + lingua + '.png" alt="' + lingua + '" class="bandierina">';
+      console.log("bandiera");
+    }
+    return lingua
+
+  }
+  // stelline
+  function starVote(voto) {
+    var stellinaVoto = []
+    votoFilmStelle = Math.ceil(voto/2)
+    for (var y = 0; y < 5; y++) {
+      if (votoFilmStelle > y) {
+        stellinaVoto.push("fas")
+      }else {
+        stellinaVoto.push("far")
+      }
+    }
+    return stellinaVoto
+  }
+  // immagine non trovata
+  function Imagenotfound() {
+
   }
 })
