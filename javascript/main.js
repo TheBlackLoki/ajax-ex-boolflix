bfbdd3869d7a2f0b338785c426dc5d02cf4b65a1
Luca Stefani <theblackloki@gmail.com>
2020-04-26 18:18:32 +0200

    Milestone 4

--- javascript/main.js
+++ javascript/main.js
@@ -1,117 +1,115 @@
 $(document).ready(function(){
-  // // var Handlebars
+
+  // // var Handlebars--------------------------------
+
   var source = $("#templateFilm").html();
   var template = Handlebars.compile(source);
+
+// --------------------------------------------
+
   $("#boton").click(function () {
     $(".libreriaFilm").html("")
     $(".libreriaSerie").html("")
-    var film_serieCercato = $("#zerca").val()
+    var urlFilm = "https://api.themoviedb.org/3/search/movie"
+    var urlTelefilm = "https://api.themoviedb.org/3/search/tv"
+    var apiKey = "a85d7660d72355c1cdf03fd5b8318e9f"
+    var film_serieCercato = $("#cerca").val()
     console.log(film_serieCercato);
-
-
-    $.ajax({
-      url:"https://api.themoviedb.org/3/search/movie",
-      method:"GET",
-      data:{
-        api_key:"a85d7660d72355c1cdf03fd5b8318e9f",
-        query: film_serieCercato,
-        language:"it-IT"
-      },
-      success: function (data,stato) {
-        console.log(data);
-        var listaFilmSerie = data.results
-        console.log(listaFilmSerie);
-        generaFilm(listaFilmSerie)
-
-      },
-      error:function (richiesta,stato,errore) {
-        alert("Chiamata fallita!")
-      }
-    })
-
-
-    $.ajax({
-      url:"https://api.themoviedb.org/3/search/tv",
-      method:"GET",
-      data:{
-        api_key:"a85d7660d72355c1cdf03fd5b8318e9f",
-        query: film_serieCercato,
-        language:"it-IT"
-      },
-      success: function (data,stato) {
-        console.log(data);
-        var listaFilmSerie = data.results
-        console.log(listaFilmSerie);
-        generaFilm(listaFilmSerie)
-
-      },
-      error:function (richiesta,stato,errore) {
-        alert("Chiamata fallita!")
-      }
-    })
+    chiamataAjax(urlFilm,apiKey,film_serieCercato,"film",".libreriaFilm")
+    chiamataAjax(urlTelefilm,apiKey,film_serieCercato,"serie",".libreriaSerie")
   })
 
+
+
+  // chiamata ajax -------------------------------------
+
+  function chiamataAjax(url,apiKey,query,tipo,dove) {
+    $.ajax({
+      url:url,
+      method:"GET",
+      data:{
+        api_key:apiKey,
+        query: query,
+        language:"it-IT"
+      },
+      success: function (data,stato) {
+        console.log(data);
+        var listaFilmSerie = data.results
+        console.log(listaFilmSerie);
+        generaFilm(listaFilmSerie)
+        if (data.total_results==0) {
+          $(dove).append("non ci sono " + tipo + " con questo nome")
+        };
+        $(".info").hide()
+        $(".copertina").mouseover(function () {
+          $(this).hide()
+          $(this).next().show()
+        })
+        $(".info").mouseout(function () {
+          $(this).hide()
+          $(this).prev().show()
+        })
+
+      },
+      error:function (richiesta,stato,errore) {
+        alert("Chiamata fallita!")
+      }
+    })
+  }
+
+// genera film ----------------------------------------------
 
   function generaFilm(film) {
     for (var i = 0; i < film.length; i++) {
       var filmserie = film[i]
       // serie o film
       if (typeof filmserie.name == 'undefined') {
-        var copertina = filmserie.poster_path
-
-        var context = {
-          tipo:"del film",
-          title:filmserie.title,
-          originalTitle:filmserie.original_title,
-          language:lingua(filmserie.original_language),
-          vote:starVote(filmserie.vote_average),
-          sitocopertina:'src="https://image.tmdb.org/t/p/',
-          dimensionecopertina:"w185/",
-          immaginecopertina:copertina
-        }
-        if (copertina === null) {
-          context.sitocopertina = 'class="notfound"src ="img/notfound.jpg" alt="image not found"'
-          context.dimensionecopertina = ""
-        }
-        console.log("sono un film");
-        console.log(context);
-        var html = template(context)
-        $(".libreriaFilm").append(html)
+        compilazioneTemplate(filmserie,"Film",filmserie.title,filmserie.original_title,".libreriaFilm")
       }else {
-        var copertina = filmserie.poster_path
-        var context = {
-          tipo:"della serie",
-          title:filmserie.name,
-          originalTitle:filmserie.original_name,
-          language:lingua(filmserie.original_language),
-          vote:starVote(filmserie.vote_average),
-          sitocopertina:'src="https://image.tmdb.org/t/p/',
-          dimensionecopertina:"w185/",
-          immaginecopertina:copertina
-        }
-        if (copertina === null) {
-          context.sitocopertina = 'class="notfound"src ="img/notfound.jpg" alt="image not found"'
-          context.dimensionecopertina = ""
-        }
-        console.log("sono una serie");
-        console.log(context);
-        var html = template(context)
-        $(".libreriaSerie").append(html)
+        compilazioneTemplate(filmserie,"Serie",filmserie.name,filmserie.original_name,".libreriaSerie")
       }
 
     }
   }
-  // bandierine
+
+  // compilazione template------------------------------------
+
+  function compilazioneTemplate(richiesta,tipo,titolo,titoloOriginale,dove) {
+    var copertina = richiesta.poster_path
+    var context = {
+      tipo:tipo,
+      title:titolo,
+      originalTitle:titoloOriginale,
+      language:lingua(richiesta.original_language),
+      vote:starVote(richiesta.vote_average),
+      sitocopertina:'https://image.tmdb.org/t/p/',
+      dimensionecopertina:"w342/",
+      immaginecopertina:copertina,
+      overview:richiesta.overview
+    }
+    if (copertina === null) {
+      context.sitocopertina = 'img/notfound.jpg" alt="image not found"'
+      context.dimensionecopertina = ""
+    }
+    var html = template(context)
+    $(dove).append(html)
+    }
+
+  // bandierine ------------------------------------------
+
+
   function lingua(lingua) {
     var bandieraLingua = ["en","it","de","es","cn","pt","jp"]
     if (bandieraLingua.includes(lingua)) {
       return '<img src="img/' + lingua + '.png" alt="' + lingua + '" class="bandierina">';
-      console.log("bandiera");
     }
     return lingua
-
   }
-  // stelline
+
+
+  // stelline -----------------------------------------
+
+
   function starVote(voto) {
     var stellinaVoto = []
     votoFilmStelle = Math.ceil(voto/2)
@@ -124,8 +122,6 @@ $(document).ready(function(){
     }
     return stellinaVoto
   }
-  // immagine non trovata
-  function Imagenotfound() {
 
-  }
+
 })
